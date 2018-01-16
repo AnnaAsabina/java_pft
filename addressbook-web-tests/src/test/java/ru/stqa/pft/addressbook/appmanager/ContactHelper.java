4dbb8e3b6699f92f48c2f3869d0a180dde62e17e
AnnaAsabina <a.asabina@quality-lab.ru>
2018-01-16 23:20:06 +0300

    Реализована возможность запускать тесты под разными браузерами

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -1,13 +1,14 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import ru.stqa.pft.addressbook.model.ContactData;
 
 
 public class ContactHelper extends HelperBase{
 
-  public ContactHelper(FirefoxDriver wd) {
+  public ContactHelper(WebDriver wd) {
     super(wd);
   }
 
@@ -15,6 +16,7 @@ public class ContactHelper extends HelperBase{
   { type(By.name("firstname"), objectData.getName());
     type(By.name("middlename"), objectData.getMiddleName());
     type(By.name ("lastname"), objectData.getLastName());
+
   }
 
   public void submitEdition(){
