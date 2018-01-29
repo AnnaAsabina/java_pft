d662679c61266f3857cec5d69896ec61817170b5
AnnaAsabina <a.asabina@quality-lab.ru>
2018-01-29 22:32:03 +0300

    Добавлена проверка предусловий для перехода на страницу создания контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -41,6 +41,11 @@ public class ContactHelper extends HelperBase {
   }
 
   public void gotoNewAddressPage() {
+    if (isElementPresent(By.tagName("h1"))
+            && wd.findElement(By.tagName("h1")).getText().equals("Edit / add address book entry"))
+            {
+      return;
+    }
     click(By.linkText("add new"));
   }
 
