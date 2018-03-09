204d2b839502cdaedc5b5a156ab15c8207bf9fff
AnnaAsabina <a.asabina@quality-lab.ru>
2018-03-10 01:07:21 +0300

    Изменение процесса запуска браузера
    Измененние на  @BeforeSuite и  @AfterSuite
    
    ссылка на ApplicationManager теперь общая для всех запускаемых тестов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class ContactCreationTests extends TestBase {
 
 
-  @Test
+  @Test(enabled = false)
   public void testCroupCreation() {
     app.getNavigationHelper().gotoHomePage();
     List<ContactData> before = app.getContactHelper().getContactList();
