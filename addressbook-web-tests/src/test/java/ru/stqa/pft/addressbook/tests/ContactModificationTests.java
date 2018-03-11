42000a63a42930519353895ed2d56f5beb5f9d62
AnnaAsabina <a.asabina@quality-lab.ru>
2018-03-11 23:36:29 +0300

    Улучшена архитектура тестов ContactModification и ContactDeletion
    Добавлены BeforeMethod для тестов ContactModification и ContactDeletion
    Добавлен метод modifyGroup в тест ContactModification

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -11,7 +11,7 @@ import java.util.HashSet;
 import java.util.List;
 
 public class ContactModificationTests extends  TestBase {
-  @Test(enabled = false)
+  @Test (enabled = false)
   public void testContactModification() {
     app.getNavigationHelper().gotoHomePage();
     if(!app.getContactHelper().isThereAContact()){
@@ -20,7 +20,7 @@ public class ContactModificationTests extends  TestBase {
     List<ContactData> before = app.getContactHelper().getContactList();
     app.getContactHelper().CheckTheContact(before.size()-1);
     app.getContactHelper().initContactModification();
-    ContactData contact = new ContactData(before.get(before.size()-1).getId(),"Anna", "Pavlovna", "Asabina", null);
+    ContactData contact = new ContactData(before.get(before.size()-1).getId(),"Sergey", "Pavlovna", "Asabina", null);
     app.getContactHelper().fillContactForm((contact), false);
     app.getContactHelper().submitContactModification();
     app.getNavigationHelper().gotoHomePage();
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
@@ -8,23 +9,28 @@ import java.util.List;
 
 public class GroupDeletionTests extends TestBase {
 
-    
+  public class GroupModificationTests extends TestBase {
+
+    @BeforeMethod
+    public void ensurePreconditions() {
+      app.getNavigationHelper().gotoGroupPage();
+      if (!app.getGroupHelper().isThereAGroup()) {
+        app.getGroupHelper().CreateGroup(new GroupData("test1", "test2", "test3"));
+      }
+    }
+
     @Test
     public void testGroupDeletion() {
-
-       app.getNavigationHelper().gotoGroupPage();
-       if (! app.getGroupHelper().isThereAGroup()){
-         app.getGroupHelper().CreateGroup(new GroupData("test1", "test2", "test3"));
-       }
       List<GroupData> before = app.getGroupHelper().getGroupList();
-        app.getGroupHelper().selectGroup(before.size()-1);
-        app.getGroupHelper().deleteSelectedGroups();
-        app.getGroupHelper().returnToGroupPage();
+      app.getGroupHelper().selectGroup(before.size() - 1);
+      app.getGroupHelper().deleteSelectedGroups();
+      app.getGroupHelper().returnToGroupPage();
       List<GroupData> after = app.getGroupHelper().getGroupList();
-      Assert.assertEquals(after.size(),before.size() -1);
+      Assert.assertEquals(after.size(), before.size() - 1);
 
-      before.remove(before.size()-1);
-      Assert.assertEquals(before,after);
+      before.remove(before.size() - 1);
+      Assert.assertEquals(before, after);
     }
-    }
+  }
+}
 
